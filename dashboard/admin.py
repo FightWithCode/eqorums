6ccea9e8644fd17f83662ccf664f5e3d7574037c
FightWithCode <rahulsharmanoj@gmail.com~>
2024-02-12 16:45:48 +0530

    changes

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,14 @@
+# django imports
 from django.contrib import admin
-from dashboard.models import Profile, EmailTemplate
+
+# models import
+from dashboard.models import (
+    Profile, 
+    EmailTemplate, 
+    ProTip
+)
 
 
 admin.site.register(Profile)
 admin.site.register(EmailTemplate)
+admin.site.register(ProTip)
