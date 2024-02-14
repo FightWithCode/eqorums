d635d1183838b77b95345fe3d6ce0e86563931ce
FightWithCode <rahulsharmanoj@gmail.com>
2024-02-14 14:30:57 +0530

    changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -8985,7 +8985,8 @@ class UserProfileAPI(APIView):
 					print(e)
 					response['profile_photo'] = profile_obj.profile_photo
 			else:
-				response['profile_photo'] = profile_obj.profile_photo.url
+				# response['profile_photo'] = profile_obj.profile_photo.url
+				response['profile_photo'] = None
 			return Response(response, status=status.HTTP_200_OK)
 		except Exception as e:
 			response['msg'] = 'error'
