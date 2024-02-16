a5e75b9b337f517b9521388466e40276856d8caf
FightWithCode <rahulsharmanoj@gmail.com>
2024-02-16 14:45:28 +0530

    added aws utils

---
+++ demo/aws_utils.py
@@ -0,0 +1,23 @@
+# python imports
+import boto3
+from botocore.exceptions import ClientError
+
+# django imports
+from django.conf import settings
+
+
+aws_access_key = settings.AWS_ACCESS_KEY_ID
+aws_secret_key = settings.AWS_SECRET_ACCESS_KEY
+bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+
+def delete_image(file_location):
+    try:
+        s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
+        
+        # Delete the file from S3 bucket
+        s3.delete_object(Bucket=bucket_name, Key=file_location)
+        return True
+    except Exception as e:
+        print(f"Error deleting file from S3: {e}")
+        return False
+
