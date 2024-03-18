ca63866b4e2b58acc748eda157ef182d1dfb983b
FightWithCode <FightWithCode>
2024-03-18 18:47:31 +0530

    changes

--- demo/custom_middlewares.py
+++ demo/custom_middlewares.py
@@ -20,8 +20,11 @@ class UserLogMiddleware:
         response = self.get_response(request)
         
         # logic that needs to be executed before the view is called  equivalent to process_response
-        user_obj = User.objects.get(id=response.data.get("user"))
-        if request.path_info in ACTIVITY_URLS and response.status_code in [200, 201]:
+        try:
+            user_obj = User.objects.get(id=response.data.get("user"))
+        except:
+            user_obj = None
+        if user_obj and request.path_info in ACTIVITY_URLS and response.status_code in [200, 201]:
             UserActivity.objects.create(
                 user=user_obj,
                 activity_name=response.data.get("msg"),
