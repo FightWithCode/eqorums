4b7cc5ad81606f9d4405fc6562dde0373c6348cd
FightWithCode <FightWithCode>
2024-03-19 12:08:46 +0530

    changes

--- schedules/models.py
+++ schedules/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 class CronofyAuthCode(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    access_token = models.CharField(max_lenght=255)
-    refresh_token = models.CharField(max_lenght=255)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
     sub = models.CharField(max_length=255)
     account_id = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now=True)
